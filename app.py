b4b8ef32d6f4e332d62f3d4d7c2be44535d4937b
Harshil-Mehta-13 <107093673+Harshil-Mehta-13@users.noreply.github.com>
2025-09-19 21:46:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ st.set_page_config(
 @st.cache_resource(ttl="30s") # Cache connection for 30 seconds
 def init_connection():
     # Correctly access secrets using their variable names
-    url = st.secrets["https://ynodggqmitbqluwmljjg.supabase.co/rest/v1/air_compressor"]
-    key = st.secrets["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlub2RnZ3FtaXRicWx1d21sampnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0NzE1MDcsImV4cCI6MjA3MDA0NzUwN30.LTf5dUJL3Y4-bofHZ-pZ1mWAv60gX0FDON5uIzjgCWM"]
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
     return create_client(url, key)
 
 supabase_client = init_connection()
